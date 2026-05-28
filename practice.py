026390906f1b31a04266620ebb60ff88b180e491
mwangisam203 <mwangisam203@gmail.com>
2026-05-28 06:53:24 -0400

    snips

--- practice.py
+++ practice.py
@@ -9,4 +9,10 @@ two_d_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(two_d_list)
 
 my_array_numb = np.array(my_list)
+my_array_numb2 = np.array(my_list, dtype=float)
+
 print(my_array_numb)
+print(my_array_numb2)
+
+array_to_list = my_array_numb.tolist()
+print(array_to_list)
