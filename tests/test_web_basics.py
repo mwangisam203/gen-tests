e0f9371d3f504a8ffe16bea39dcc18385b30db9e
mwangisam203 <mwangisam203@gmail.com>
2026-07-02 12:54:57 -0400

    test web parsing helpers

---
+++ tests/test_web_basics.py
@@ -0,0 +1,25 @@
+from learning_lab.web_basics import link_texts, page_title
+
+
+HTML = """
+<html>
+  <head><title>Practice Page</title></head>
+  <body>
+    <a href="/one">One</a>
+    <a href="/two">Two</a>
+    <a href="/empty"></a>
+  </body>
+</html>
+"""
+
+
+def test_page_title_extracts_text():
+    assert page_title(HTML) == "Practice Page"
+
+
+def test_page_title_returns_empty_string_when_missing():
+    assert page_title("<main>No title</main>") == ""
+
+
+def test_link_texts_ignores_empty_links():
+    assert link_texts(HTML) == ["One", "Two"]
