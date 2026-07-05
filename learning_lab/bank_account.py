1031906a2a32ec9ef8cb9c8959077340804de13e
mwangisam203 <mwangisam203@gmail.com>
2026-07-05 14:50:22 -0400

    add balance transfer helper

--- learning_lab/bank_account.py
+++ learning_lab/bank_account.py
@@ -22,3 +22,10 @@ def withdraw(balance: float, amount: float) -> float:
 def apply_interest(balance: float, annual_rate: float) -> float:
     """Apply one year of simple interest."""
     return round(balance * (1 + annual_rate), 2)
+
+
+def transfer(source_balance: float, target_balance: float, amount: float) -> tuple[float, float]:
+    """Move money from one balance to another."""
+    new_source = withdraw(source_balance, amount)
+    new_target = deposit(target_balance, amount)
+    return new_source, new_target
