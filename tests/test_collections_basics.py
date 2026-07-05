e60a65c14d8ff252acb7b56a5f4b8f4412080d59
mwangisam203 <mwangisam203@gmail.com>
2026-07-05 14:50:22 -0400

    test flatten once helper

--- tests/test_collections_basics.py
+++ tests/test_collections_basics.py
@@ -1,6 +1,6 @@
 import pytest
 
-from learning_lab.collections_basics import count_words, only_passing, top_student
+from learning_lab.collections_basics import count_words, flatten_once, only_passing, top_student
 
 
 def test_only_passing_filters_scores():
@@ -18,3 +18,7 @@ def test_top_student_returns_highest_score():
 def test_top_student_rejects_empty_scores():
     with pytest.raises(ValueError):
         top_student({})
+
+
+def test_flatten_once_combines_nested_lists():
+    assert flatten_once([["a", "b"], ["c"]]) == ["a", "b", "c"]
