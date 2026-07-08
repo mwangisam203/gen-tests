6b0739137cce64f2f492848cf4ebe4192993bbbf
mwangisam203 <mwangisam203@gmail.com>
2026-07-08 03:53:32 -0400

    practicing gobal code

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 # Import the requests library so we can send HTTP requests to websites
-import requests
+# import requests
 
-# Import BeautifulSoup so we can parse and search through HTML later
-from bs4 import BeautifulSoup
+# # Import BeautifulSoup so we can parse and search through HTML later
+# from bs4 import BeautifulSoup
 
-# Store the website URL we want to request
-url = "https://www.livescore.com/en/"
+# # Store the website URL we want to request
+# url = "https://www.livescore.com/en/"
 
-# Send a GET request to the website and store the response
-response = requests.get(url)
+# # Send a GET request to the website and store the response
+# response = requests.get(url)
 
 
-# Get the HTTP status code from the response
-status = response.status_code
+# # Get the HTTP status code from the response
+# status = response.status_code
 
-content = response.content
-soup = BeautifulSoup(content, "html.parser")
+# content = response.content
+# soup = BeautifulSoup(content, "html.parser")
 
-# Print the status code to the terminal
-print(status)
-print(soup.title)
-print(soup.title.get_text)
-print(soup.body)  # gives the whole page on the website
+# # Print the status code to the terminal
+# print(status)
+# print(soup.title)
+# print(soup.title.get_text)
+# print(soup.body)  # gives the whole page on the website
 
+y = " great"
+def myfunc():
+    y = "fabulous"
+    print("my day was" + y)
+
+myfunc()
+print(f"my day was{y}")
 
 
