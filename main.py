74e0a39dff9ce1b41bd5c2c8e851ede77f35a994
mwangisam203 <mwangisam203@gmail.com>
2026-07-13 06:38:50 -0400

    test

--- main.py
+++ main.py
@@ -23,144 +23,153 @@
 # print(soup.title.get_text)
 # print(soup.body)  # gives the whole page on the website
 
-y = " great"
-def myfunc():
-    y = "fabulous"
-    print("my day was" + y)
+# y = " great"
+# def myfunc():
+#     y = "fabulous"
+#     print("my day was" + y)
 
-myfunc()
-print(f"my day was {y}")
+# myfunc()
+# print(f"my day was {y}")
 
-def global_func():
-    global x
-    x = "interesting"
+# def global_func():
+#     global x
+#     x = "interesting"
 
-global_func()
-print(f"I had a quite an {x} day")
+# global_func()
+# print(f"I had a quite an {x} day")
 
-#test addition
-def add(a, b):
-    return a + b
-
-
-#test math utils
-from math import add
+# #test addition
+# def add(a, b):
+#     return a + b
 
 
-def test_add_two_positive_numbers():
-    assert add(2, 3) == 5
 
 
-def test_add_negative_numbers():
-    assert add(-2, -5) == -7
+# def test_add_two_positive_numbers():
+#     assert add(2, 3) == 5
 
 
-def test_add_zero():
-    assert add(0, 7) == 7
+# def test_add_negative_numbers():
+#     assert add(-2, -5) == -7
 
 
-def reverse(text):
-    return text[::-1]
+# def test_add_zero():
+#     assert add(0, 7) == 7
+
+
+# def reverse(text):
+#     return text[::-1]
 
 
 #Testing String Reversal :from strings import reverse
 
 
-def test_reverse_word():
-    assert reverse("python") == "nohtyp"
+# def test_reverse_word():
+#     assert reverse("python") == "nohtyp"
 
 
-def test_reverse_empty_string():
-    assert reverse("") == ""
+# def test_reverse_empty_string():
+#     assert reverse("") == ""
 
 
-def test_reverse_single_letter():
-    assert reverse("A") == "A"
+# def test_reverse_single_letter():
+#     assert reverse("A") == "A"
 
 
-#even number
-def is_even(number):
-    return number % 2 == 0
+# #even number
+# def is_even(number):
+#     return number % 2 == 0
 
-from numbers import is_even
-
-
-def test_even_number():
-    assert is_even(10)
+# from numbers import is_even
 
 
-def test_odd_number():
-    assert not is_even(9)
+# def test_even_number():
+#     assert is_even(10)
 
 
-def test_zero_is_even():
-    assert is_even(0)
-
-#email validation test
-def is_valid_email(email):
-    return "@" in email and "." in email
-
-#from validation import is_valid_email
+# def test_odd_number():
+#     assert not is_even(9)
 
 
-def test_valid_email():
-    assert is_valid_email("john@gmail.com")
+# def test_zero_is_even():
+#     assert is_even(0)
+
+# #email validation test
+# def is_valid_email(email):
+#     return "@" in email and "." in email
+
+# #from validation import is_valid_email
 
 
-def test_missing_at_symbol():
-    assert not is_valid_email("johngmail.com")
+# def test_valid_email():
+#     assert is_valid_email("john@gmail.com")
 
 
-def test_missing_dot():
-    assert not is_valid_email("john@gmail")
-
-#number sorting
-def sort_numbers(numbers):
-    return sorted(numbers)
+# def test_missing_at_symbol():
+#     assert not is_valid_email("johngmail.com")
 
 
-def test_sort_random_numbers():
-    assert sort_numbers([5, 1, 3]) == [1, 3, 5]
+# def test_missing_dot():
+#     assert not is_valid_email("john@gmail")
+
+# #number sorting
+# def sort_numbers(numbers):
+#     return sorted(numbers)
 
 
-def test_sort_empty_list():
-    assert sort_numbers([]) == []
+# def test_sort_random_numbers():
+#     assert sort_numbers([5, 1, 3]) == [1, 3, 5]
 
 
-def test_sort_duplicates():
-    assert sort_numbers([3, 2, 3]) == [2, 3, 3]
+# def test_sort_empty_list():
+#     assert sort_numbers([]) == []
 
 
-#testing bank acc
-class BankAccount:
-
-    def __init__(self):
-        self.balance = 0
-
-    def deposit(self, amount):
-        self.balance += amount
-
-def test_initial_balance():
-    account = BankAccount()
-    assert account.balance == 0
+# def test_sort_duplicates():
+#     assert sort_numbers([3, 2, 3]) == [2, 3, 3]
 
 
-def test_deposit():
-    account = BankAccount()
-    account.deposit(500)
-    assert account.balance == 500
+# #testing bank acc
+# class BankAccount:
+
+#     def __init__(self):
+#         self.balance = 0
+
+#     def deposit(self, amount):
+#         self.balance += amount
+
+# def test_initial_balance():
+#     account = BankAccount()
+#     assert account.balance == 0
 
 
-#dict test
-def create_user(name, age):
-    return {
-        "name": name,
-        "age": age
-    }
+# def test_deposit():
+#     account = BankAccount()
+#     account.deposit(500)
+#     assert account.balance == 500
 
-def test_create_user():
-    user = create_user("Sam", 25)
 
-    assert user["name"] == "Sam"
-    assert user["age"] == 25
+# #dict test
+# def create_user(name, age):
+#     return {
+#         "name": name,
+#         "age": age
+#     }
 
+# def test_create_user():
+#     user = create_user("Sam", 25)
+
+#     assert user["name"] == "Sam"
+#     assert user["age"] == 25
+
+
+
+numbers = [10, 8, 32, 12, 25, 78, 41, 13, 6, 1, 5]
+
+smallest_number = numbers[0]
+
+for i in numbers:
+    if i < smallest_number:
+        smallest_number = i
+
+print("smallest number: ", smallest_number)
